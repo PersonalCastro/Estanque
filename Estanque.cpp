109beec2f6791b68f896268548b7ae64aa19451e
PersonalCastro <pablo.cassal2000@gmail.com>
2019-08-10 16:06:33 +0200

    Documentacion y estructura del programa

---
+++ Estanque.cpp
@@ -0,0 +1,30 @@
+/**
+ * 	Name: Estanque
+ *	Author: PersonalCastro
+ *	Brief: (traducir al inglés) Estealgoritmo esta basado en el algoritmode ordenacion llamado "burbuja"
+ *		su principal problema esel tiempo estimado de duración y hepensado que si se mejoraba de alguna forma
+ *		podria dar buenos resultados.
+ *		Este consta de su movimiento principal donde (A,B) "A" y "B" son cambiados de posición en cuanto uno de estos
+ *		lo requiere (ya dependiendo de la referencia de ordenacionque se tenga en ese momento).
+ *		La formadde mejorarlo estara en que cuando se efectue este intercambio se llamara a una funció recursiva alterna
+ *		que comprobara si se puede efectuar ese mismo movimiento pero en sentido contrario.
+ *		
+ *		"Lo que mas me gusto, fue poder ver todas esas ondas creadas a partir de una sola piedrecita".
+ *
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+	cout << "Se procede a crear un vector aleatorio con un tamaño predeterminado." << endl;
+
+	cout << "Se pone el algoritmo para que ordene el vector y se mide el tiempo de proceso." << endl;
+
+	cout << "Sacamos por pantalla el vector (el resultado)."  endl;
+
+	cout << "Comprobamos si hay algun error poniendo en verde lo que este bien y en rojo lo que no." << endl;
+
+}
